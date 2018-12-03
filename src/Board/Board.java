69831a3377bcbf4f545cfc18340cb411da7e00df
BT_1N3_02 <wchen1325@bths.edu>
2018-12-03 14:45:19 -0500

    k

---
+++ src/Board/Board.java
@@ -0,0 +1,22 @@
+package Board;
+
+import Rooms.Room;
+
+public class Board {
+    private Room[][] board;
+
+    public Board (Room[][] board){
+        this.board= board;
+    }
+
+    public String toString() {
+        String str = "";
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                str = str + board[i][j];
+            }
+            str = str + "\n";
+        }
+        return str;
+    }
+}

5a38136fea17dcf251aa668036d97e4d8ad56529
wchen1325 <wchen1325@bths.edu>
2018-12-16 17:04:49 -0500

    ok

--- src/Board/Board.java
+++ src/Board/Board.java
@@ -24,28 +24,8 @@ public class Board {
         return str;
     }
 
-    /*public void print(){
-        String str = "";
-
-        for ( int i = 0; i < board.length;i++){
-            for(int j = 0; j<board[i].length; j++){
-                str += board[i][j].toString();
-            }
-            str += "\n";
-        }
-        System.out.println(str);
-    }*/
-
 
     public void generateMaze(){
-        /*int x = (int)(Math.random()*board.length);
-        int y = (int)(Math.random()*board.length);
-
-        while(x < 2 || x > board.length - 2 || y < 2 || y > board.length - 2 || x%2 == 1 || y % 2 ==1){
-            x = (int)(Math.random()*board.length);
-            y = (int)(Math.random()*board.length);
-        }
-        */
         wallList = new int[board.length][board.length];
 
         //3 == Cell marked
@@ -132,62 +112,7 @@ public class Board {
 
 
         }
-        /*
-        wallList[x][y] = 3;
-        wallList[x+2][y]= 1;
-        wallList[x][y+2]=1;
-        wallList[x-2][y]=1;
-        wallList[x][y-2]=1;
-        int wallCount = 1;
 
-
-
-        while (wallCount > 0){
-            int randomWall = (int)(Math.random()*wallCount);
-
-            for (int i = 0; i < wallList.length; i++){
-                for (int j = 0; j < wallList[i].length;j++){
-                    if (wallList[i][j] == 1){
-                        if(randomWall == 0){
-                            wallList[i][j] = 3;
-                            if(i < wallList.length-1) {
-                                wallList[i+1][j] = 3;
-                                wallList[i+2][j]++;
-                            }
-                            if(j < wallList.length-1) {
-                                wallList[i][j+1] = 3;
-                                wallList[i][j+2]++;
-                            }
-                            if(i > 1) {
-                                wallList[i-1][j] = 3;
-                                wallList[i-2][j]++;
-                            }
-                            if(j > 1) {
-                                wallList[i][j-1] = 3;
-                                wallList[i][j-2]++;
-                            }
-                        }
-                        else{
-                            randomWall--;
-                        }
-
-                    }
-                }
-            }
-
-            //check if there are still walls in the list
-            wallCount = 0;
-            for ( int i = 0; i < wallList.length; i++)
-            {
-                for ( int j = 0; j < wallList[i].length; j++)
-                {
-                    if(wallList[i][j] == 1){
-                        wallCount++;
-                    }
-                }
-            }
-        }
-        */
 
         //build the board base on the wallList
         for (int i = 0; i < board.length; i++)
